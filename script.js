b73df917c82b784143d0e337f5ce62fe289e49fa
unknown <rahulgbgautam@gmail.com>
2021-10-31 11:25:18 +0530

    f comit

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizDB = [
+	{
+		question:"Q1:Inside which HTML element do we put the JavaScript?",
+		a:"<javascript>",
+		b:"<js>",
+		c:"<src>",
+		d:"<script>",
+		ans:a
+	},
+	{
+		question:"Q2:Where is the correct place to insert a JavaScript?",
+		a:"Both the head section and the body section are correct",
+		b:"The head section",
+		c:"The body section",
+		d:"None of the above",
+		ans:a
+	},
+	{
+		question:"Q3:Is it necessary for the external script file to contain a <script> tag?",
+		a:"Yes",
+		b:"NO",
+		c:"Depends on the type of include",
+		d:"None of the above",
+		ans:a
+	},
+	{
+		question:"Q4:What is the correct syntax for referring to an external script called 'gfg.js'?",
+		a:"<script name='gfg.js'>",
+		b:"<script href='gfg.js'>",
+		c:"<script src='gfg.js'>",
+		d:"None of these",
+		ans:a
+	},
+	{
+		question:"Q5:How many ways are there with which we can declare a variable in javascript?",
+		a:"Only one",
+		b:"Three",
+		c:"Infinitely many",
+		d:"None of the above",
+		ans:a
+	}
+]
